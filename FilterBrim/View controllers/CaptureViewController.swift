c540ff306ac6bad972e0f22d26aa812b15d9ddfa
Claudiu Miron <shout@claudiumiron.ro>
2020-05-28 11:48:23 +0300

    Hook up initial Capture.storyboard view controller to CaptureViewController class

---
+++ FilterBrim/View controllers/CaptureViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CaptureViewController.swift
+//  FilterBrim
+//
+//  Created by Claudiu Miron on 28/05/2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class CaptureViewController: UIViewController {
+    
+}
