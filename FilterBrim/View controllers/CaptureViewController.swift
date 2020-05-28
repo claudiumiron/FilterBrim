d7a08a7bf9e2fb7b0c22c63dab13be1177627a7f
Claudiu Miron <shout@claudiumiron.ro>
2020-05-28 15:54:52 +0300

    Add video feed preview

--- FilterBrim/View controllers/CaptureViewController.swift
+++ FilterBrim/View controllers/CaptureViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2020 Apple. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class CaptureViewController: UIViewController {
+    
+    private let session = AVCaptureSession()
     
     private enum SessionSetupResult {
         case success
@@ -21,7 +24,24 @@ class CaptureViewController: UIViewController {
     // Communicate with the session and other session objects on this queue.
     private let sessionQueue = DispatchQueue(label: "SessionQueue", attributes: [], autoreleaseFrequency: .workItem)
     
-    @IBOutlet weak var videoView: UIView!
+    private let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,
+                                                                                             .builtInWideAngleCamera,
+                                                                                             .builtInTelephotoCamera,
+                                                                                             .builtInTrueDepthCamera],
+                                                                               mediaType: .video,
+                                                                               position: .unspecified)
+    
+    private let audioDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInMicrophone],
+                                                                               mediaType: .audio,
+                                                                               position: .unspecified)
+    
+    private var videoInput: AVCaptureDeviceInput!
+    
+    private let videoFileOutput = AVCaptureMovieFileOutput()
+    
+    private var isSessionRunning = false
+    
+    @IBOutlet private weak var videoView: UIView!
     @IBOutlet private weak var resumeButton: UIButton!
     @IBOutlet private weak var cameraUnavailableLabel: UILabel!
     @IBOutlet private weak var recordButton: UISwitch!
@@ -58,6 +78,157 @@ class CaptureViewController: UIViewController {
             // The user has previously denied access
             setupResult = .notAuthorized
         }
+        
+        /*
+         Setup the capture session.
+         In general it is not safe to mutate an AVCaptureSession or any of its
+         inputs, outputs, or connections from multiple threads at the same time.
+         
+         Don't do this on the main queue, because AVCaptureSession.startRunning()
+         is a blocking call, which can take a long time. Dispatch session setup
+         to the sessionQueue so as not to block the main queue, which keeps the UI responsive.
+         */
+        sessionQueue.async {
+            self.configureSession()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        sessionQueue.async {
+            switch self.setupResult {
+            case .success:
+                self.session.startRunning()
+                
+                DispatchQueue.main.async {
+                    self.setupLivePreview(session: self.session)
+                }
+                
+            case .notAuthorized:
+                DispatchQueue.main.async {
+                    let message = NSLocalizedString("AVCamFilter doesn't have permission to use the camera, please change privacy settings",
+                                                    comment: "Alert message when the user has denied access to the camera")
+                    let actions = [
+                        UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"),
+                                      style: .cancel,
+                                      handler: nil),
+                        UIAlertAction(title: NSLocalizedString("Settings", comment: "Alert button to open Settings"),
+                                      style: .`default`,
+                                      handler: { _ in
+                                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!,
+                                                                  options: [:],
+                                                                  completionHandler: nil)
+                        })
+                    ]
+                    
+                    self.alert(title: "FilterBrim", message: message, actions: actions)
+                }
+                
+            case .configurationFailed:
+                DispatchQueue.main.async {
+                    
+                    let message = NSLocalizedString("Unable to capture media",
+                                                    comment: "Alert message when something goes wrong during capture session configuration")
+                    
+                    self.alert(title: "FilterBrim",
+                               message: message,
+                               actions: [UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"),
+                                                       style: .cancel,
+                                                       handler: nil)])
+                }
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        sessionQueue.async {
+            if self.setupResult == .success {
+                self.session.stopRunning()
+                self.isSessionRunning = self.session.isRunning
+            }
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    // MARK: - Screen rendering
+    
+    func setupLivePreview(session: AVCaptureSession) {
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        videoView.layer.addSublayer(videoPreviewLayer)
+        videoPreviewLayer.frame = videoView.bounds
+    }
+    
+    // MARK: - Session management
+    
+    private func configureSession() {
+        if setupResult != .success {
+            return
+        }
+        
+        let defaultVideoDevice = videoDeviceDiscoverySession.devices.first
+        
+        guard let videoDevice = defaultVideoDevice else {
+            print("Could not find any video device")
+            setupResult = .configurationFailed
+            return
+        }
+        
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoDevice) else {
+            print("Could not create video device input")
+            setupResult = .configurationFailed
+            return
+        }
+        self.videoInput = videoInput
+        
+        session.beginConfiguration()
+        
+        session.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        // Add a video input.
+        guard session.canAddInput(videoInput) else {
+            print("Could not add video device input to the session")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
+            return
+        }
+        session.addInput(videoInput)
+        
+        guard session.canAddOutput(videoFileOutput) else {
+            print("Could not add video file output to the session")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
+            return
+        }
+        session.addOutput(videoFileOutput)
+        
+        if let audioDevice = audioDeviceDiscoverySession.devices.first {
+            if let audioInput = try? AVCaptureDeviceInput(device: audioDevice) {
+                if session.canAddInput(audioInput) {
+                    session.addInput(audioInput)
+                }
+            }
+        }
+        
+        session.commitConfiguration()
+    }
+    
+    // MARK: - Utilities
+    
+    func alert(title: String, message: String, actions: [UIAlertAction]) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        
+        actions.forEach {
+            alertController.addAction($0)
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
     
     // MARK: - IBActions
     
