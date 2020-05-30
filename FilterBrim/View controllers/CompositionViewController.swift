96b036b9d16056c57c31717f281977c745eb8f6c
Claudiu Miron <shout@claudiumiron.ro>
2020-05-30 17:17:32 +0300

    Add code to resize foreground video and set transparency

--- FilterBrim/View controllers/CompositionViewController.swift
+++ FilterBrim/View controllers/CompositionViewController.swift
@@ -17,6 +17,8 @@ class CompositionViewController: UIViewController {
     
     private let composition = AVMutableComposition()
     
+    private let videoComposition = AVMutableVideoComposition()
+    
     private var bgAsset: AVAsset?
     
     private var fgAsset: AVAsset?
@@ -59,47 +61,79 @@ class CompositionViewController: UIViewController {
                 bgAsset.duration : fgAsset.duration
         
         let compBgVideoTrack = composition.addMutableTrack(withMediaType: .video,
-                                                           preferredTrackID: 1)!
+                                                           preferredTrackID: 2)!
         let assetBgVideoTrack = bgAsset.firstVideoTrack!
-        compBgVideoTrack.preferredTransform = assetBgVideoTrack.preferredTransform
-        let compBgAudioTrack = composition.addMutableTrack(withMediaType: .audio,
-                                                           preferredTrackID: 1)!
-        let assetBgAudioTrack = bgAsset.firstAudioTrack!
+        //compBgVideoTrack.preferredTransform = assetBgVideoTrack.preferredTransform
         try! compBgVideoTrack.insertTimeRange(timeRange,
                                               of: assetBgVideoTrack,
                                               at: .zero)
-        try! compBgAudioTrack.insertTimeRange(timeRange,
-                                              of: assetBgAudioTrack,
-                                              at: .zero)
         
         let compFgVideoTrack = composition.addMutableTrack(withMediaType: .video,
-                                                           preferredTrackID: 2)!
+                                                           preferredTrackID: 1)!
         let assetFgVideoTrack = fgAsset.firstVideoTrack!
-        compFgVideoTrack.preferredTransform = assetFgVideoTrack.preferredTransform
-        let compFgAudioTrack = composition.addMutableTrack(withMediaType: .audio,
-                                                           preferredTrackID: 2)!
-        let assetFgAudioTrack = fgAsset.firstAudioTrack!
+        //compFgVideoTrack.preferredTransform = assetFgVideoTrack.preferredTransform
         try! compFgVideoTrack.insertTimeRange(timeRange,
                                               of: assetFgVideoTrack,
                                               at: .zero)
-        try! compFgAudioTrack.insertTimeRange(timeRange,
-                                              of: assetFgAudioTrack,
-                                              at: .zero)
+        
+        let naturalSize = assetBgVideoTrack.naturalSize
+        let transformedSize =
+            naturalSize.applying(assetBgVideoTrack.preferredTransform)
+        let renderSize = CGSize(width: abs(transformedSize.width),
+                                height: abs(transformedSize.height))
+        
+        videoComposition.renderSize = renderSize
+        videoComposition.frameDuration =
+            CMTimeMakeWithSeconds(Float64(1.0 / assetBgVideoTrack.nominalFrameRate),
+                                  preferredTimescale: assetBgVideoTrack.naturalTimeScale);
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = composition.tracks.first!.timeRange
+        
+        let fgInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: assetFgVideoTrack)
+        fgInstruction.trackID = 1
+        
+        let bgInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: assetBgVideoTrack)
+        bgInstruction.trackID = 2
+        
+        let scale: CGFloat = 0.5
+        var fgTransform = assetFgVideoTrack.preferredTransform
+        fgTransform = fgTransform.scaledBy(x: scale, y: scale)
+        
+        fgInstruction.setOpacity(0.7, at: .zero)
+        fgInstruction.setTransform(fgTransform, at: .zero)
+        
+        let bgTransform = assetBgVideoTrack.preferredTransform
+        bgInstruction.setTransform(bgTransform, at: .zero)
+        
+        instruction.layerInstructions = [fgInstruction, bgInstruction]
+        videoComposition.instructions = [instruction]
         
         let playerItem = AVPlayerItem(asset: composition)
+        //playerItem.customVideoCompositor
+        playerItem.videoComposition = videoComposition
+        
         let player = AVQueuePlayer(playerItem: playerItem)
         playerLayer = AVPlayerLayer(player: player)
+        playerLayer!.videoGravity = .resizeAspect
+        playerLayer!.backgroundColor = UIColor.green.cgColor
         DispatchQueue.main.async {
             self.previewView.layer.addSublayer(self.playerLayer!)
         }
         playerLooper = AVPlayerLooper(player: player,
                                       templateItem: playerItem)
+        playerLooper.status
         
         player.play()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        playerLayer?.frame = previewView.bounds
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         playerLayer?.frame = previewView.bounds
     }
     
